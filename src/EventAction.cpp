bff0972a890c3a3539ace20f0e43e0cb74616ef1
Justo Martin-Albo <jmalbos@users.noreply.github.com>
2019-08-15 15:47:04 -0400

    Added run and event action classes.

---
+++ src/EventAction.cpp
@@ -0,0 +1,29 @@
+// -----------------------------------------------------------------------------
+//  G4Basic | EventAction.cpp
+//
+//
+//   * Author: Justo Martin-Albo
+//   * Creation date: 15 Aug 2019
+// -----------------------------------------------------------------------------
+
+#include "EventAction.hh"
+
+
+EventAction::EventAction(): G4UserEventAction()
+{
+}
+
+
+EventAction::~EventAction()
+{
+}
+
+
+void EventAction::BeginOfEventAction(const G4Event*)
+{
+}
+
+
+void B1EventAction::EndOfEventAction(const G4Event*)
+{
+}
---
+++ src/RunAction.cpp
@@ -0,0 +1,29 @@
+// -----------------------------------------------------------------------------
+//  G4Basic | RunAction.h
+//
+//
+//   * Author: Justo Martin-Albo
+//   * Creation date: 15 Aug 2019
+// -----------------------------------------------------------------------------
+
+#include "RunAction.h"
+
+
+B1RunAction::B1RunAction(): G4UserRunAction()
+{
+}
+
+
+B1RunAction::~B1RunAction()
+{
+}
+
+
+void B1RunAction::BeginOfRunAction(const G4Run*)
+{
+}
+
+
+void B1RunAction::EndOfRunAction(const G4Run* run)
+{
+}
