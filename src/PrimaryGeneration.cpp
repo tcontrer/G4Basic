1ac946910cc40c310d339d0693220d0a9c25dcde
Justo Martin-Albo <jmalbos@users.noreply.github.com>
2019-08-14 22:58:55 -0400

    First commit of primary generation class.

---
+++ src/PrimaryGeneration.cpp
@@ -0,0 +1,24 @@
+// -----------------------------------------------------------------------------
+//  G4Basic | PrimaryGeneration.cpp
+//
+//  Class for the definition of the primary generation action.
+//   * Author: Justo Martin-Albo
+//   * Creation date: 14 Aug 2019
+// -----------------------------------------------------------------------------
+
+#include "PrimaryGeneration.h"
+
+
+PrimaryGeneration::PrimaryGeneration(): G4VUserPrimaryGeneratorAction()
+{
+}
+
+
+PrimaryGeneration::~PrimaryGeneration()
+{
+}
+
+
+void PrimaryGeneration::GeneratePrimaries(G4Event*)
+{
+}
---
+++ src/PrimaryGeneration.h
@@ -0,0 +1,23 @@
+// -----------------------------------------------------------------------------
+//  G4Basic | PrimaryGeneration.h
+//
+//  Class for the definition of the primary generation action.
+//   * Author: Justo Martin-Albo
+//   * Creation date: 14 Aug 2019
+// -----------------------------------------------------------------------------
+
+#ifndef PRIMARY_GENERATION_H
+#define PRIMARY_GENERATION_H
+
+#include <G4VUserPrimaryGeneratorAction.hh>
+
+
+class PrimaryGeneration: public G4VUserPrimaryGeneratorAction
+{
+public:
+  PrimaryGeneration();
+  virtual ~PrimaryGeneration();
+  virtual void GeneratePrimaries(G4Event*);
+};
+
+#endif
